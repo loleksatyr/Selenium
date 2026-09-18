52ccfdca63b586a3182b1011feb0a69cf0df5a5d
loleksatyr <czarnotabartek@gmail.com>
2026-09-18 23:32:27 +0200

    bump

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,6 +1,8 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
@@ -9,8 +11,17 @@ public class BaseTest {
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
+        System.out.println(driver.getTitle());
 
 
+
+    }
+    public static void main(String[] args){
+        BaseTest test = new BaseTest();
+        test.setUp();
     }
 
 }
