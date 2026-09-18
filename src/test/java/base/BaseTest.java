8255dfc403a388d3222908d7a826188274a0b7d8
loleksatyr <czarnotabartek@gmail.com>
2026-09-18 22:14:28 +0200

    Initial commit

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,16 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+    private WebDriver driver;
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+
+
+    }
+
+}
